73d4b68c6ee66e9a14e0389cef3dbf628813e8f5
mathiaskammoun_local <mathias.kammoun@gmail.com>
2023-11-07 17:15:31 +0200

    implemented classes for POM and a function to try to get the code right before incorporating it in the classes

--- pages/welcome_page.py
+++ pages/welcome_page.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.by import By
 
 
 class WelcomePage:
@@ -5,3 +6,21 @@ class WelcomePage:
     def __init__(self, driver):
         self.driver = driver
 
+    def is_title_matches(self):
+        """Verifies that the hardcoded text "lusha" appears in page title"""
+
+        return "Lusha" in self.driver.title
+
+    def click_form_button(self):
+        """Triggers the search"""
+
+        element = self.driver.find_element(*WelcomePageLocators.CONTACT_SALES_BUTTON)
+        element.click()
+
+
+class WelcomePageLocators(object):
+    """A class for main page locators. All main page locators should come here"""
+
+    # CONTACT_SALES_BUTTON = (By.CLASS_NAME, "app btn js-contact-form-btn contact-sales-btn d-lo d-li-none m-0")
+    # CONTACT_SALES_BUTTON = (By.CLASS_NAME, "app contact-sales js-contact-form-btn btn d-li d-lo-none animate__fadeIn animate__animated animate__faster")
+    CONTACT_SALES_BUTTON = (By.ID, 'header-form-contact-sales-visitors-button')
--- tests/test_welcome.py
+++ tests/test_welcome.py
@@ -1,4 +1,78 @@
+from os.path import dirname
+import json
+
 from selenium import webdriver
 
+import unittest
 
-driver = webdriver.Chrome()
+from pages.welcome_page import WelcomePage
+
+with open(dirname(__file__) + "/../config.json", "r") as jsonfile:
+    CONFIG = json.load(jsonfile)
+
+
+class Lusha_ContactSales(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get(CONFIG["welcome_page"])
+
+    def test_contactSalesForm(self):
+        """
+        Test lusha.com contact sales feature
+        :return:
+        """
+        # Load the main page. In this case the home page of Python.org.
+        welcome_page = WelcomePage(self.driver)
+        # Checks if the word "Python" is in title
+        self.assertTrue(welcome_page.is_title_matches(), "Lusha title doesn't match.")
+        welcome_page.click_form_button()
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def code():
+
+    # Set Chrome options for headless mode
+    chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument("--headless")
+
+    # Initialize the Chrome driver with options
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(CONFIG["welcome_page"])
+
+    welcome_page = WelcomePage(driver)
+
+    # tags = (By.CLASS_NAME, "app.btn.js-contact-form-btn.contact-sales-btn.d-lo.d-li-none.m-0")
+    tags = (By.XPATH, "//button[contains(@class, 'app btn js-contact-form-btn contact-sales-btn d-lo d-li-none m-0') and @data-test-id='header-form-contact-sales-visitors-button']")
+
+    # button_element = driver.find_element(*tags)
+    button_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located(tags))
+    button_element.click()
+
+    wait = WebDriverWait(driver, 10)
+    form_element = wait.until(EC.presence_of_element_located((By.ID, "cf7mls-next-btn-cf7mls_step-1")))
+
+    # Check if the form is displayed
+    if form_element.is_displayed():
+        print("The form is open.")
+    else:
+        print("The form did not open as expected.")
+
+
+if __name__ == "__main__":
+    # unittest.main()
+
+    code()
+
+
+
+
+
+
