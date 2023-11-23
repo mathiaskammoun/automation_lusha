a755806c8c9c960f2f25278c2b500a13be0b2f2a
mathiaskammoun_local <mathias.kammoun@gmail.com>
2023-11-23 16:01:55 +0200

    implemented code to fill forms - need to implement test cases

--- pages/welcome_page.py
+++ pages/welcome_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-
+from selenium.webdriver.support.ui import Select
 
 import time
 
@@ -28,9 +28,38 @@ class WelcomePage(BasePage):
         actions = ActionChains(self.driver)
         # Perform the click action on the button
         actions.double_click(button_element).perform()
+        # Wait to make sure the form is opened
+        wait.until(EC.visibility_of_element_located(ContactSalesFormLocators.CONTACT_SALES_FORM))
 
-        form_element = wait.until(EC.presence_of_element_located(ContactSalesFormLocators.CONTACT_SALES_FORM))
-        print(form_element.is_displayed())
+
+class WelcomePageContactForm(BasePage):
+
+    def is_form_opened(self):
+        """ """
+        form_element = self.driver.find_element(*ContactSalesFormLocators.CONTACT_SALES_FORM)
+        return form_element.is_displayed()
+
+    def fill_form_p1(self, form_p1_keys):
+        # form_p1_keys[""]
+        Select(self.driver.find_element(*ContactSalesFormLocators.P1_NUMBER_USERS)).select_by_visible_text(form_p1_keys["n_users"])
+        Select(self.driver.find_element(*ContactSalesFormLocators.P1_NUMBER_PROSPECTS)).select_by_visible_text(form_p1_keys["n_prospects"])
+        Select(self.driver.find_element(*ContactSalesFormLocators.P1_NUMBER_EMPLOYEES)).select_by_visible_text(form_p1_keys["n_employees"])
+        Select(self.driver.find_element(*ContactSalesFormLocators.P1_COMPANY_INDUSTRY)).select_by_visible_text(form_p1_keys["industry"])
+
+        self.driver.find_element(*ContactSalesFormLocators.P1_POSITION).send_keys(form_p1_keys["position"])
+        self.driver.find_element(*ContactSalesFormLocators.P1_NEXT_BUTTON).click()
+
+    def fill_form_p2(self, form_p2_keys):
+        # Wait to make sure the form is opened
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.visibility_of_element_located(ContactSalesFormLocators.P2_SUBMIT_BUTTON))
+
+        self.driver.find_element(*ContactSalesFormLocators.P2_FNAME).send_keys(form_p2_keys["fname"])
+        self.driver.find_element(*ContactSalesFormLocators.P2_LNAME).send_keys(form_p2_keys["lname"])
+        self.driver.find_element(*ContactSalesFormLocators.P2_EMAIL).send_keys(form_p2_keys["email"])
+        self.driver.find_element(*ContactSalesFormLocators.P2_PHONE_NUMBER).send_keys(form_p2_keys["phone"])
+        self.driver.find_element(*ContactSalesFormLocators.P2_HELP_BOX).send_keys(form_p2_keys["help"])
+        time.sleep(3)
 
 
 class WelcomePageLocators:
@@ -41,5 +70,21 @@ class WelcomePageLocators:
 
 class ContactSalesFormLocators:
 
-    # CONTACT_SALES_FORM = (By.ID, "cf7mls-next-btn-cf7mls_step-1")
     CONTACT_SALES_FORM = (By.XPATH, "//div[contains(@class,'lusha-popup__container p-0')]")
+
+    P1_NUMBER_USERS = (By.ID, "inputUsers")
+    P1_NUMBER_PROSPECTS = (By.ID, "inputAvg")
+    P1_NUMBER_EMPLOYEES = (By.ID, "inputEmployees")
+    P1_COMPANY_INDUSTRY = (By.ID, "inputIndustry")
+    P1_POSITION = (By.ID, "inputPosTxt")
+    P1_NEXT_BUTTON = (By.ID, "cf7mls-next-btn-cf7mls_step-1")
+
+    P2_FNAME = (By.ID, "inputFname")
+    P2_LNAME = (By.ID, "inputLname")
+    P2_EMAIL = (By.ID, "inputEmail")
+    P2_PHONE_INDICATOR = ()
+    P2_PHONE_NUMBER = (By.ID, "inputPhone")
+    P2_HELP_BOX = (By.NAME, "pricing_contact_us_details")
+    P2_SUBMIT_BUTTON = (By.XPATH, "//input[contains(@class,'wpcf7-form-control wpcf7-submit')]")
+
+
--- tests/test_welcome.py
+++ tests/test_welcome.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 
 import unittest
 
-from pages.welcome_page import WelcomePage
+from pages.welcome_page import WelcomePage, WelcomePageContactForm
 
 with open(dirname(__file__) + "/../config.json", "r") as jsonfile:
     CONFIG = json.load(jsonfile)
@@ -26,7 +26,19 @@ class Lusha_ContactSales(unittest.TestCase):
         welcome_page = WelcomePage(self.driver)
         # Checks if the word "Python" is in title
         self.assertTrue(welcome_page.is_title_matches(), "Lusha title doesn't match.")
-        welcome_page.open_contact_sales_form()
+
+        contact_form = WelcomePageContactForm(self.driver)
+        # Checks if the form opened
+        self.assertTrue(contact_form.is_form_opened())
+
+        form_p1_keys = {"n_users": "1-5", "n_prospects": "400-1000",
+                        "n_employees": "201-500", "industry": "Industrials", "position": "Sales"}
+        contact_form.fill_form_p1(form_p1_keys)
+
+        form_p2_keys = {"fname": "Mickey", "lname": "Mouse", "email": "m_mouse@gmail.com",
+                        "phone": "343-245-3453", "help": "Thank You!"}
+        contact_form.fill_form_p2(form_p2_keys)
+
 
     def tearDown(self):
         self.driver.quit()
@@ -44,7 +56,16 @@ def code():
 
     welcome_page = WelcomePage(driver)
     welcome_page.open_contact_sales_form()
+    contact_form = WelcomePageContactForm(driver)
+    assert contact_form.is_form_opened()
 
+    form_p1_keys = {"n_users": "1-5", "n_prospects": "400-1000",
+                    "n_employees": "201-500", "industry": "Industrials", "position": "Sales"}
+    contact_form.fill_form_p1(form_p1_keys)
+
+    form_p2_keys = {"fname": "Mickey", "lname": "Mouse", "email": "m_mouse@gmail.com",
+                    "phone": "343-245-3453", "help": "Thank You!"}
+    contact_form.fill_form_p2(form_p2_keys)
 
 
 if __name__ == "__main__":
